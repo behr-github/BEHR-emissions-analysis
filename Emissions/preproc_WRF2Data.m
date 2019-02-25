d2e3ae7c686eb96b74cf4505242c4c9dd8ec861a
Josh Laughner <first.tempora@gmail.com>
2019-02-24 16:29:29 -0800

    Rewrote preproc_WRF2Data again to store individual WRF "swaths"

--- Emissions/preproc_WRF2Data.m
+++ Emissions/preproc_WRF2Data.m
@@ -30,37 +30,52 @@ p.addParameter('avg_levels', 'all');
 variables = pout.variables;
 avg_levels = pout.avg_levels;
 
+data_vars = veccat({'Date', 'Longitude', 'Latitude', 'FoV75CornerLongitude', 'FoV75CornerLatitude', 'Areaweight'}, variables);
+
 avg_proc.no2_vcd = struct('variables', {'no2', 'pres'}, 'proc_fxn', @wrf_no2_vcd);
 
 dvec = make_datevec(start_dates, end_dates);
 WRFFiles = BEHRMatchedWRFFiles('region', 'us');
 for d=1:numel(dvec)
     fprintf('Working on %s\n', datestr(dvec(d)));
-    day_avg = wrf_time_average(dvec(d), dvec(d), variables, 'processing', avg_proc, 'matched_wrf_files', WRFFiles);
-    [xloncorn, xlatcorn] = wrf_grid_corners(results.XLONG, results.XLAT);
-    Data = struct('Latitude', day_avg.XLONG, 'Longitude', day_avg.XLAT,...
-        'FoV75CornerLongitude', xloncorn, 'FoV75CornerLatitude', xlatcorn,...
-        'Areaweight', ones(size(day_avg.XLONG)));
-    for i_var = 1:numel(variables)
-        this_var = variables{i_var};
-        value = day_avg.(this_var);
-        if ndims(value) > 3
-            E.notimplemented('Value averaged from WRF files has >3 dimensions');
-        elseif ~ismatrix(value) % if 3d
-            if ischar(avg_levels) && strcmpi(avg_levels, 'all')
-                levels = 1:size(value,3);
-            elseif ischar(avg_levels)
-                E.badinput('The only string recognized for "avg_levels" is "all"')
+    [todays_wrf_files, behr_data] = WRFFiles.get_files_for_date(dvec(d));
+    Data = repmat(make_empty_struct_from_cell(data_vars), size(behr_data));
+    for i_orbit = 1:numel(behr_data)
+        swath_lon_edge = edge_to_vector(behr_data(i_orbit).Longitude);
+        swath_lat_edge = edge_to_vector(behr_data(i_orbit).Latitude);
+        wrf_lon = ncread(todays_wrf_files{i_orbit}, 'XLONG');
+        wrf_lat = ncread(todays_wrf_files{i_orbit}, 'XLAT');
+        [wrf_loncorn, wrf_latcorn] = wrf_grid_corners(wrf_lon, wrf_lat);
+        
+        xx = inpolygon(wrf_lon, wrf_lat, swath_lon_edge, swath_lat_edge);
+        Data(i_orbit).Date = datestr(dvec(d));
+        Data(i_orbit).Longitude = wrf_lon(xx);
+        Data(i_orbit).Latitude = wrf_lat(xx);
+        Data(i_orbit).FoV75CornerLongitude = wrf_loncorn(:,xx);
+        Data(i_orbit).FoV75CornerLatitude = wrf_latcorn(:,xx);
+        Data(i_orbit).Areaweight = ones(size(Data(i_orbit).Longitude));
+        for i_var = 1:numel(variables)
+            this_var = variables{i_var};
+            if isfield(avg_proc, this_var)
+                WRF = read_wrf_vars('', todays_wrf_files{i_orbit}, avg_proc.(this_var).variables, 'squeeze', 'as_struct');
+                value = avg_proc.(this_var).proc_fxn(WRF);
             else
-                levels = avg_levels;
+                value = read_wrf_preproc(todays_wrf_files{i_orbit}, this_var);
             end
-            if ~isempty(levels)
-                value = nanmean(value(:,:,avg_levels), 3);
+            
+            if ndims(value) > 3
+                E.notimplemented('WRF array with > 3 dimensions')
+            elseif ischar(avg_levels) && strcmpi(avg_levels, 'all')
+                value = nanmean(value, 3);
+            elseif ischar(avg_levels)
+                E.badinput('The only recognized string for "avg_levels" is "all"')
+            else
+                value = nanmean(value(:,:,avg_levels),3);
             end
+            
+            Data(i_orbit).(this_var) = value;
         end
-        Data.(this_var) = value;
     end
-    
     save_name = sprintf('WRF_PseudoBEHR_%04d%02d%02d.mat',year(dvec(d)),month(dvec(d)),day(dvec(d)));
     fprintf('    Saving %s\n',fullfile(save_path,save_name));
     save(fullfile(save_path,save_name),'Data');
